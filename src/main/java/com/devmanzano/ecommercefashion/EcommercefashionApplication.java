a8ff854d7377b7d6fc40aa862997cb27b1ce1763
Felipe Manzano <felipemanzanofp@gmail.com>
2022-03-07 19:38:16 -0300

    criação do projeto

---
+++ src/main/java/com/devmanzano/ecommercefashion/EcommercefashionApplication.java
@@ -0,0 +1,13 @@
+package com.devmanzano.ecommercefashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercefashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercefashionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmanzano/ecommercefashion/EcommercefashionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmanzano.ecommercefashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercefashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
