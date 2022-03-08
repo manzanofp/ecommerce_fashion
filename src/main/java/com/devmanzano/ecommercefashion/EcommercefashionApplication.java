6361accbcd8a105f8505163ce84667b6dac8dc28
Felipe Manzano <felipemanzanofp@gmail.com>
2022-03-08 12:04:22 -0300

    Criando operação de instanciação

--- src/main/java/com/devmanzano/ecommercefashion/EcommercefashionApplication.java
+++ src/main/java/com/devmanzano/ecommercefashion/EcommercefashionApplication.java
@@ -1,13 +1,30 @@
 package com.devmanzano.ecommercefashion;
 
+import com.devmanzano.ecommercefashion.model.Categoria;
+import com.devmanzano.ecommercefashion.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class EcommercefashionApplication {
+public class EcommercefashionApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommercefashionApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
 }
